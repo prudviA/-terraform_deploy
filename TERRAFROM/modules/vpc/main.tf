6eec8b32013de3da3376bc9d90382ab085303ff9
prudviA <165309155+prudviA@users.noreply.github.com>
2025-03-23 14:05:59 +0530

    Update main.tf

--- TERRAFROM/modules/vpc/main.tf
+++ TERRAFROM/modules/vpc/main.tf
@@ -49,6 +49,6 @@ resource "aws_route_table" "public_route_table" {
 }
 
 resource "aws_route_table_association" "public_subnet_association" {
-  subnet_id      = public_subnet
+  subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_route_table.id
 }
