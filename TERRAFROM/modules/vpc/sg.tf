05c49a3ac45ffb7e0b067bdf5f44ee24ee2b181e
prudviA <165309155+prudviA@users.noreply.github.com>
2025-03-23 15:13:18 +0530

    Update sg.tf

--- TERRAFROM/modules/vpc/sg.tf
+++ TERRAFROM/modules/vpc/sg.tf
@@ -37,7 +37,7 @@ resource "aws_security_group" "rds-sg" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   # Egress Rule: Allow all outbound traffic for IPv6
