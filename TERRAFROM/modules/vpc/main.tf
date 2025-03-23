b72539ca4b2c5785e8f99e5c0c2077bc9a8b2988
prudviA <165309155+prudviA@users.noreply.github.com>
2025-03-23 14:19:54 +0530

    Update main.tf

--- TERRAFROM/modules/vpc/main.tf
+++ TERRAFROM/modules/vpc/main.tf
@@ -39,7 +39,7 @@ resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.terraform_vpc.id
 
   route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 
