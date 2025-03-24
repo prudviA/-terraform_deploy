b4ea79200ea8901ed8bdd68af556ae00cb6c789c
prudviA <165309155+prudviA@users.noreply.github.com>
2025-03-24 18:15:13 +0530

    Update output.tf

--- TERRAFROM/modules/vpc/output.tf
+++ TERRAFROM/modules/vpc/output.tf
@@ -14,7 +14,7 @@ output "private_subnet_id" {
 }
 
 output "ec2_public_ip" {
-  value = aws_instance.web_server.public_ip.id
+  value = aws_instance.web_server.public_ip
   description = "The public IP address of the EC2 instance."
 }
 
