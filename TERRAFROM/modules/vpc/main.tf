9d813f55fbc311a590abfa5321c0263228fea9c8
prudviA <165309155+prudviA@users.noreply.github.com>
2025-03-24 18:27:22 +0530

    Update main.tf

--- TERRAFROM/modules/vpc/main.tf
+++ TERRAFROM/modules/vpc/main.tf
@@ -11,6 +11,7 @@ resource "aws_subnet" "public_subnet" {
   vpc_id     = aws_vpc.terraform_vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "public_subnet"
