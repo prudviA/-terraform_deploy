cd13d330fa662bb535530601a2b46e3998db7528
prudviA <165309155+prudviA@users.noreply.github.com>
2025-03-24 18:28:21 +0530

    Update ec2.tf

--- TERRAFROM/modules/vpc/ec2.tf
+++ TERRAFROM/modules/vpc/ec2.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "web_server" {
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.ec2-sg.id]
+  associate_public_ip_address = true
     tags = {
     Name = "web_server"
   }
